bf03f42cb48531450d38a24f2a8ba1b99718cfe7
fgiongo <fgiongo@protonmail.com>
2025-05-03 19:18:28 -0300

    ex01 completo

--- ex01/innerprod.c
+++ ex01/innerprod.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <pthread.h>
 
 
@@ -19,6 +20,15 @@ typedef struct {
 } file_data;
 
 
+typedef struct {
+    int t_index;
+    int n_threads;
+    int v_dim;
+    double * v1;
+    double * v2;
+} thread_arg;
+
+
 cmdl_args parse_cmdl_args( int argc, char * argv[] );
 file_data read_file( char * path );
 int int_read ( FILE * fp );
@@ -26,6 +36,8 @@ double * vec_read( int n, FILE * fp );
 double double_read( FILE * fp );
 void vec_print( double * v, int n );
 double vec_inner_product( double * u, double * v, int n );
+double var_relativa( double c, double s );
+void * inner_product_worker( void * a );
 
 
 int main( int argc, char * argv[] ) {
@@ -37,21 +49,64 @@ int main( int argc, char * argv[] ) {
     pthread_t * tid   = malloc( sizeof( pthread_t ) * args.n_threads );
     thread_arg * t_arg = malloc( sizeof( thread_arg ) * args.n_threads );
     if ( tid == NULL || t_arg == NULL ) {
-        fprintf( stderr, "create_threads: memory allocation error\n" );
+        fprintf( stderr, "Memory allocation error, exiting\n" );
         exit( 1 );
     }
-    for ( int i = 0; i < n_threads; ++i ) {
-        t_arg[ i ].index = i;
-        t_arg[ i ].v1    = v1;
-        t_arg[ i ].v2    = v2;
-        t_arg[ i ].dim   = dim;
-        int err = pthread_create( &tid[i], NULL, inner_product_worker, ( void * ) &t_arg[ i ] );
+    for ( int i = 0; i < args.n_threads; ++i ) {
+        fprintf( stderr, "Creating worker thread #%d...\n", i + 1 );
+        t_arg[ i ].t_index = i;
+        t_arg[ i ].n_threads = args.n_threads;
+        t_arg[ i ].v_dim = input.dim;
+        t_arg[ i ].v1 = input.v1;
+        t_arg[ i ].v2 = input.v2;
+        int err = pthread_create( &tid[ i ], NULL, inner_product_worker, ( void * ) &t_arg[ i ] );
         if ( err != 0 ) {
-            fprintf( stderr, "thread creation error\n" );
+            fprintf( stderr, "Thread creation error, exiting\n" );
             exit( 1 );
         }
     }
+    double result = 0;
+    for ( int i = 0; i < args.n_threads; ++i ) {
+        fprintf( stderr, "Waiting for worker thread #%d...\n", i + 1 );
+        double * retval;
+        int err = pthread_join( tid[ i ], ( void ** )&retval );
+        if ( err != 0 ) {
+            fprintf( stderr, "Thread termination error\n" );
+            exit( 1 );
+        }
+        double partial = * retval;
+        fprintf( stderr, "Worker #%d has returned %.16lf\n", i + 1, partial );
+        result += partial;
+    }
+    fprintf( stderr, "Final result: %.16lf, expected: %.16lf\n", result, input.inner_product );
+    fprintf( stderr, "Var. Relativa: %.16lf\n", var_relativa( result, input.inner_product ) );
     return 0;
+}
+
+
+void * inner_product_worker( void * a ) {
+    thread_arg * arg = ( thread_arg * )a;
+    double * result = malloc( sizeof( double ) );
+    if ( result == NULL ) {
+        fprintf( stderr, "Worker #%d: could not allocate memory for result\n", arg->t_index);
+    }
+    int size = arg->v_dim / arg->n_threads;
+    int startpos  = size * arg->t_index;
+    int endpos = startpos + size;
+    if ( arg->t_index == arg->n_threads - 1 ) {
+        endpos = arg->v_dim;
+    }
+    fprintf( stderr, "Worker #%d of %d: working on vector of size %d, slice size: %d, start pos: %d, end pos: %d\n",
+            arg->t_index + 1, arg->n_threads, arg->v_dim, size, startpos, endpos);
+    for ( int i = startpos; i < endpos; ++i ) {
+        * result += arg->v1[ i ] * arg->v2[ i ];
+    }
+    pthread_exit( ( void * )result );
+}
+
+
+double var_relativa( double c, double s ) {
+    return fabs( ( s - c ) / s );
 }
 
 
@@ -101,7 +156,7 @@ int int_read ( FILE * fp ) {
     int elem_read = fread(&n, sizeof( int ), 1, fp);
     if ( elem_read != 1 ) {
         fprintf( stderr, "int_read: erro de leitura de arquivo\n" );
-        fclose(fp);
+        fclose( fp );
         exit( 1 );
     }
     return n;
